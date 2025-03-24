c2a6118ea34ebeee43619496375305a56609af22
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-03-24 16:17:37 +0100

    functions syu: added flatpak.

--- functions/syu.fish
+++ functions/syu.fish
@@ -1,6 +1,7 @@
 function syu
   sudo pacman --noconfirm --needed -Syyu
   yay --noconfirm --needed -Syu
+  sudo flatpak --system --assumeyes --noninteractive
   sudo pacman --noconfirm -Rs "$(pacman -Qdtq)"
   sudo pacman --noconfirm -Sc "$(pacman -Qdtq)"
 end
