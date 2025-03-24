5481fb70d1b81f997d21a199faf5e4deefe10101
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-03-24 06:48:20 +0100

    fishline: initial commit.

--- config.fish
+++ config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set FLINE_PATH $HOME/.config/fish/plugins/fishline
+
+    # fishline
+    source $FLINE_PATH/init.fish
 end
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,4 @@
+function fish_prompt
+  # fishline
+  fishline -s $status CLOCK PWD GIT STATUS WRITE ROOT SPACE
+end
