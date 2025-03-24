888773a4d94fe9098ec59a651f7dbc9c256517f5
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-03-24 16:26:15 +0100

    functions syu: added echo comments for clarity.

--- functions/syu.fish
+++ functions/syu.fish
@@ -1,7 +1,16 @@
 function syu
+  echo "update via pacman"
   sudo pacman --noconfirm --needed -Syyu
+
+  echo "\nupdate via yay"
   yay --noconfirm --needed -Syu
+
+  echo "\nupdate via flatpak"
   sudo flatpak update --system --assumeyes --noninteractive
+
+  echo "\nremoving unneeded packages via pacman"
   sudo pacman --noconfirm -Rs "$(pacman -Qdtq)"
+
+  echo "\ncleaning downloaded packages via pacman"
   sudo pacman --noconfirm -Sc "$(pacman -Qdtq)"
 end
