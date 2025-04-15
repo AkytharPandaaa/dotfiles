7a28b23cf3d7fa6f454ea26ba4586f59d9b5d9bb
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-04-15 15:54:17 +0200

    functions zmkfwu: changed to function that auto-updates zmk firmware.zip for the repo you are in.

--- functions/zmkfwu.fish
+++ functions/zmkfwu.fish
@@ -1,3 +1,10 @@
-function gll
-  git log --pretty=format:"%h" | head -n1
+function zmkfwu
+  set git_hash $(git log --pretty=format:"%h" | head -n1)
+
+  git rm firmware_*.zip
+  mv -v ~/Downloads/firmware.zip ./firmware_$git_hash.zip
+  git add firmware_$git_hash.zip
+  git commit -m "firmware: updated firmware to $git_hash."
+  git pull
+  git push
 end
