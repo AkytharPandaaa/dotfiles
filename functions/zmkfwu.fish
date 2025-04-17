30276659224b9244eba0bfcc0cac58db2e8eebcf
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-04-17 04:54:30 +0200

    funkctions zmkfwu: moved pull to start, to avoid constant merges.

--- functions/zmkfwu.fish
+++ functions/zmkfwu.fish
@@ -1,10 +1,10 @@
 function zmkfwu
   set git_hash $(git log --pretty=format:"%h" | head -n1)
 
+  git pull
   git rm firmware_*.zip
   mv -v ~/Downloads/firmware.zip ./firmware_$git_hash.zip
   git add firmware_$git_hash.zip
   git commit -m "firmware: updated firmware to $git_hash."
-  git pull
   git push
 end
