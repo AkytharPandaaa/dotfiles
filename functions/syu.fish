140853c9bda7f045f0f4dd94c28e5473c7e2eb19
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-05-11 16:36:14 +0200

    functions syu: fixed cleanup.

--- functions/syu.fish
+++ functions/syu.fish
@@ -1,31 +1,37 @@
 function syu
   set current_path "$(pwd)"
 
-  echo "update via pacman"
+  echo "--- update via pacman"
   sudo pacman --noconfirm --needed -Syyu
   echo ""
 
-  echo "update via yay"
+  echo "--- update via yay"
   yay --sudoloop --noconfirm --needed -Syua --cleanafter
   echo ""
 
-  echo "update via flatpak"
+  echo "--- update via flatpak"
   sudo flatpak update --system --assumeyes --noninteractive
   echo ""
+  
+  set unneeded_packages "$(sudo pacman -Qdtq)"
+  if not test -z "$unneeded_packages"
+    echo "--- removing unneeded packages via pacman"
+    sudo pacman -Qdtq | sudo pacman --noconfirm -Rsu - 
+    echo ""
+  
+    echo "--- cleaning downloaded packages via pacman"
+    sudo pacman -Qdtq | sudo pacman --noconfirm -Sc -
+    echo ""
+  else
+    echo "--- no unneeded packages"
+    echo ""
+  end
 
-  echo "removing unneeded packages via pacman"
-  sudo pacman -Qdtq | sudo pacman --noconfirm -Rsu - 
-  echo ""
-
-  echo "removing unneeded packages via yay"
+  echo "--- removing unneeded packages via yay"
   yay --sudoloop --noconfirm -Ycc
   echo ""
 
-  echo "cleaning downloaded packages via pacman"
-  sudo pacman -Qdtq | sudo pacman --noconfirm -Sc -
-  echo ""
-
-  echo "updating os-setup private repository"
+  echo "--- updating os-setup private repository"
   cd /home/os-setup/ && sudo git pull
   cd "$current_path"
 end
