94e1549fca3f655fb9393ded6abb8133b0a9dcd6
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-07-28 17:26:41 +0200

    update syu.fish
    - added apt updates to be installed if apt is available

--- functions/syu.fish
+++ functions/syu.fish
@@ -1,35 +1,49 @@
 function syu
   set current_path "$(pwd)"
 
-  echo "--- update via pacman"
-  sudo pacman --noconfirm --needed -Syyu
-  echo ""
-
-  echo "--- update via yay"
-  yay --sudoloop --noconfirm --needed -Syua --cleanafter
-  echo ""
-
-  echo "--- update via flatpak"
-  sudo flatpak update --system --assumeyes --noninteractive
-  echo ""
-  
-  set unneeded_packages "$(sudo pacman -Qdtq)"
-  if not test -z "$unneeded_packages"
-    echo "--- removing unneeded packages via pacman"
-    sudo pacman -Qdtq | sudo pacman --noconfirm -Rsu - 
+  if command -v "pacman"
+    echo "--- update via pacman"
+    sudo pacman --noconfirm --needed -Syyu
     echo ""
-  
-    echo "--- cleaning downloaded packages via pacman"
-    sudo pacman -Qdtq | sudo pacman --noconfirm -Sc -
+    
+    set unneeded_packages "$(sudo pacman -Qdtq)"
+    if not test -z "$unneeded_packages"
+      echo "--- removing unneeded packages via pacman"
+      sudo pacman -Qdtq | sudo pacman --noconfirm -Rsu - 
+      echo ""
+    
+      echo "--- cleaning downloaded packages via pacman"
+      sudo pacman -Qdtq | sudo pacman --noconfirm -Sc -
+      echo ""
+    else
+      echo "--- no unneeded packages"
+      echo ""
+    end
+  end
+
+  if command -v "yay"
+    echo "--- update via yay"
+    yay --sudoloop --noconfirm --needed -Syua --cleanafter
     echo ""
-  else
-    echo "--- no unneeded packages"
+
+    echo "--- removing unneeded packages via yay"
+    yay --sudoloop --noconfirm -Ycc
     echo ""
   end
 
-  echo "--- removing unneeded packages via yay"
-  yay --sudoloop --noconfirm -Ycc
-  echo ""
+  if command -v "apt"
+    echo "--- update via APT"
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt autoremove -y
+    sudo apt autoclean
+  end
+
+  if command -v "flatpak"
+    echo "--- update via flatpak"
+    sudo flatpak update --system --assumeyes --noninteractive
+    echo ""
+  end
 
   echo "--- updating os-setup private repository"
   cd /home/os-setup/ && sudo git pull
