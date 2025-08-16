fc5ef71eeedbc6b9a75f812498b7a3b40ff02041
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-08-16 18:53:06 +0200

    update colorscheme.lua
    - changed colorscheme to rose-pine-moon

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -43,7 +43,7 @@ return {
 					which_key = true,
 				},
 			})
-			vim.cmd([[colorscheme catppuccin]])
+			vim.cmd([[colorscheme rose-pine-moon]])
 		end,
 	},
 	{ --https://github.com/uloco/bluloco.nvim
