b8f802f9e1ce70b2db6069315aa4590e0a51e7d6
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-09-30 15:11:48 +0200

    update functions/setup_ssh_agent.fish
    - added SSH_AUTH_SOCK to systemd user environment variables
    - cosmetical changes

--- functions/setup_ssh_agent.fish
+++ functions/setup_ssh_agent.fish
@@ -12,25 +12,32 @@ function setup_ssh_agent
     sed -i "s!\(AddKeysToAgent\) .*!\1 yes" .ssh/config
   else
     echo "setting up key storage"
-    echo "AddKeysToAgent yes" | tee -a .ssh/config
+    echo "AddKeysToAgent yes" | tee -a .ssh/config > /dev/null
   end
 
   if not test -e ~/.config/systemd/user/ssh-agent.service
     echo "setting up ssh-agent service"
     touch ~/.config/systemd/user/ssh-agent.service
 
-    echo "[Unit]" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "Description=SSH key agent" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "[Service]" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "Type=simple" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "# DISPLAY required for ssh-askpass to work" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "Environment=DISPLAY=:0" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "ExecStart=/usr/bin/ssh-agent -D -a \$SSH_AUTH_SOCK" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "[Install]" | tee -a ~/.config/systemd/user/ssh-agent.service
-    echo "WantedBy=default.target" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "[Unit]" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "Description=SSH key agent" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "[Service]" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "Type=simple" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "# DISPLAY required for ssh-askpass to work" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "Environment=DISPLAY=:0" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "ExecStart=/usr/bin/ssh-agent -D -a \$SSH_AUTH_SOCK" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "[Install]" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+    echo "WantedBy=default.target" | tee -a ~/.config/systemd/user/ssh-agent.service > /dev/null
+  end
+
+  if not test -e ~/.config/environment.d/ssh-agent.conf
+    echo "setting up SSH_AUTH_SOCK"
+    touch ~/.config/environment.d/ssh-agent.conf
+    
+    echo "SSH_AUTH_SOCK=\"\${XDG_RUNTIME_DIR}/ssh-agent.socket\"" | tee -a ~/.config/environment.d/ssh-agent.conf > /dev/null
   end
 
   echo starting ssh-agent
