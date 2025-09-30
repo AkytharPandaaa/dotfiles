46dc7957b68d5fbbed5f19f6f0334d2588062cd5
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-09-30 18:56:09 +0200

    update plugins/colorscheme.lua
    - removed bluloco, vim-one
    - added makurai, everforest, kanagawa, nord, night-owl, nordic

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -43,24 +43,31 @@ return {
 					which_key = true,
 				},
 			})
-			vim.cmd([[colorscheme dracula]])
+			vim.cmd([[colorscheme rose-pine-moon]])
 		end,
 	},
-	{ --https://github.com/uloco/bluloco.nvim
-		"uloco/bluloco.nvim",
-		lazy = false,
-		priority = 1000,
-		dependencies = { "rktjmp/lush.nvim" },
-		config = function() end,
+	{ -- https://github.com/Skardyy/makurai-nvim
+		"Skardyy/makurai-nvim",
+		config = function()
+			-- you don't have to call setup
+			require("makurai").setup({
+				transparent = false, -- removes the bg color
+				bordered = false, -- removes the bg color from floats/popups
+				increase_contrast = false, -- only changes the line number and active line number for now.
+			})
+		end,
 	},
 	{ -- https://github.com/pauchiner/pastelnight.nvim
 		"pauchiner/pastelnight.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
 	},
+	{ "sainnhe/everforest" }, -- https://github.com/sainnhe/everforest
+	{ "rebelot/kanagawa.nvim" }, -- https://github.com/rebelot/kanagawa.nvim
+	{ "shaunsingh/nord.nvim" }, -- https://github.com/shaunsingh/nord.nvim
 	{ "Mofiqul/dracula.nvim" }, -- https://github.com/Mofiqul/dracula.nvim
-	{ "rakr/vim-one" }, -- https://github.com/rakr/vim-one
-	{ "rose-pine/neovim", name = "rose-pine" }, -- https://github.com/rose-pine/neovim
+	{ "oxfist/night-owl.nvim", lazy = false, priority = 1000 }, -- https://github.com/oxfist/night-owl.nvim
+	{ "AlexvZyl/nordic.nvim", lazy = false, priority = 1000 }, -- https://github.com/AlexvZyl/nordic.nvim
 	{ "perpetuatheme/nvim", name = "perpetua" }, -- https://github.com/perpetuatheme/nvim
+	{ "rose-pine/neovim", name = "rose-pine" }, -- https://github.com/rose-pine/neovim
 }
