06aa0848a06549377a18e1b92d99ac76704247fb
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-09-30 14:56:52 +0200

    initial commit: functions/setup_ssh_agent.fish

---
+++ functions/setup_ssh_agent.fish
@@ -0,0 +1,39 @@
+function setup_ssh_agent
+  # thanks to Lorenzo Bettini for the inspiration
+  # https://www.lorenzobettini.it/2023/09/hyprland-and-ssh-agent/
+
+  if test -e ~/.config/ssh
+    echo "added local ssh config"
+    touch ~/.config/ssh
+  end
+
+  if grep -q AddKeysToAgent ~/.ssh/config
+    echo "setting up key storage"
+    sed -i "s!\(AddKeysToAgent\) .*!\1 yes" .ssh/config
+  else
+    echo "setting up key storage"
+    echo "AddKeysToAgent yes" | tee -a .ssh/config
+  end
+
+  if not test -e ~/.config/systemd/user/ssh-agent.service
+    echo "setting up ssh-agent service"
+    touch ~/.config/systemd/user/ssh-agent.service
+
+    echo "[Unit]" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "Description=SSH key agent" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "[Service]" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "Type=simple" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "# DISPLAY required for ssh-askpass to work" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "Environment=DISPLAY=:0" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "ExecStart=/usr/bin/ssh-agent -D -a \$SSH_AUTH_SOCK" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "[Install]" | tee -a ~/.config/systemd/user/ssh-agent.service
+    echo "WantedBy=default.target" | tee -a ~/.config/systemd/user/ssh-agent.service
+  end
+
+  echo starting ssh-agent
+  systemctl enable --user ssh-agent.service
+  systemctl start --user ssh-agent.service
+end
