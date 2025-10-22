fb453542ced4a627023b0c22f97566cf28fc08e2
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-10-22 20:27:27 +0200

    update plugins/themery.lua: added catppuccin frappe, makurai autumn

--- lua/lukas/plugins/themery.lua
+++ lua/lukas/plugins/themery.lua
@@ -6,19 +6,20 @@ return {
 			-- add the config here
 			themes = {
 				{ name = "Catppuccin Macchiato", colorscheme = "catppuccin-macchiato" },
+				{ name = "Catppuccin Frappe", colorscheme = "catppuccin-frappe" },
 				{ name = "Dracula", colorscheme = "dracula" },
-				{ name = "Everforest", colorscheme = "everforest" },
 				{ name = "Kanagawa Wave", colorscheme = "kanagawa-wave" },
 				{ name = "Night Owl", colorscheme = "night-owl" },
-				{ name = "Nordic", colorscheme = "nordic" },
 				{ name = "Pastelnight", colorscheme = "pastelnight" },
 				{ name = "Rosé Pine Moon", colorscheme = "rose-pine-moon" },
-				{ name = "Rosé Pine", colorscheme = "rose-pine-main" },
+				{ name = "Makurai (Autumn)", colorscheme = "makurai_autumn" },
+				{ name = "Everforest", colorscheme = "everforest" },
+				{ name = "Nordic", colorscheme = "nordic" },
 				{ name = "Catppuccin Latte (light)", colorscheme = "catppuccin-latte" },
-				{ name = "Makurai (light)", colorscheme = "makurai_light" },
-				{ name = "Nord (light)", colorscheme = "nord" },
-				{ name = "Perpetua (light)", colorscheme = "perpetua-light" },
 				{ name = "Rosé Pine Dawn (light)", colorscheme = "rose-pine-dawn" },
+				{ name = "Makurai (light)", colorscheme = "makurai_light" },
+				{ name = "Perpetua (light)", colorscheme = "perpetua-light" },
+				{ name = "Nord (light)", colorscheme = "nord" },
 			},
 			livePreview = true, -- Apply theme while picking. Default to true.
 		})
