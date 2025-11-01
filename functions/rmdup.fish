5f60812a594acc360a408c316b80449912702e40
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-11-01 07:43:03 +0100

    updated functions/rmdup.fish: added info print about recursion

--- functions/rmdup.fish
+++ functions/rmdup.fish
@@ -1,4 +1,5 @@
 function rmdup
+  echo "info: this script runs recursive for subdirectories."
   echo "\$ rdfind -makeresultsfile false -followsymlinks false -ignoreempty true -deleteduplicates true $argv"
   rdfind -makeresultsfile false -followsymlinks false -ignoreempty true -deleteduplicates true "$argv"
 end
