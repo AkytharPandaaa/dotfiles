3961801b8e3aa510ebc8f3bf82e43d22d78f56ae
Lukas|@IndiePandaaaaa <indiepandaaaaa@gmx.net>
2025-11-18 14:30:59 +0100

    updated config.fish: fixed typo, added eza

--- config.fish
+++ config.fish
@@ -8,12 +8,13 @@ if status is-interactive
     # Aliases
   alias :q "exit"
   alias cp "rsync -avSAXHP"
+  alias eza "eza --icons"
   alias fixmypdf "ocrmypdf -l deu+eng+fra --rotate-pages --clean --clean-final --skip-text --optimize 2 --jpeg-quality 70 --png-quality 70 --jbig2-lossy"
 
   # git Aliases
   alias gc "git commit -m"
   alias gd "git diff"
-  alias gl "git log --pretty=format:'%h - %an : %s"
+  alias gl "git log --pretty=format:'%h - %an : %s'"
   alias gp "git pull && git push"
   alias gs "git status"
 
