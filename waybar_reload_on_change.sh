797297b93df274e0fda502276df37eca3c4f4fb2
AkytharPandaaa <akytharpandaaa@posteo.net>
2026-01-26 17:32:19 +0100

    added waybar reload script for debugging the config

---
+++ waybar_reload_on_change.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# source: https://github.com/Alexays/Waybar/issues/961#issuecomment-1792931299
+
+# start waybar if not started
+if ! pgrep -x "waybar" >/dev/null; then
+  waybar &
+fi
+
+# current checksums
+current_checksum_config=$(md5sum ~/.config/waybar/config.jsonc)
+current_checksum_style=$(md5sum ~/.config/waybar/style.css)
+
+# loop forever
+while true; do
+  # new checksums
+  new_checksum_config=$(md5sum ~/.config/waybar/config.jsonc)
+  new_checksum_style=$(md5sum ~/.config/waybar/style.css)
+
+  # if checksums are different
+  if [ "$current_checksum_config" != "$new_checksum_config" ] || [ "$current_checksum_style" != "$new_checksum_style" ]; then
+    # kill waybar
+    killall waybar
+
+    # start waybar
+    waybar &
+
+    # update checksums
+    current_checksum_config=$new_checksum_config
+    current_checksum_style=$new_checksum_style
+  fi
+
+  sleep 0.5
+
+done
