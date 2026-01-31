38b65b478de2b2cf8da9c3ef72d76eb425eced72
AkytharPandaaa <akytharpandaaa@posteo.net>
2026-01-31 17:21:03 +0100

    update config.fish: reformatted file

--- config.fish
+++ config.fish
@@ -1,22 +1,24 @@
 if status is-interactive
-# theme
-fish_config theme choose "Rosé Pine Moon Auto"
+  # theme
+  fish_config theme choose "Rosé Pine Moon Auto"
+  
+  # hydro
+  set --global hydro_symbol_prompt ">"
+  set --global hydro_symbol_git_dirty " x"
+  
+  set --global hydro_multiline true
 
-# hydro
-set --global hydro_symbol_prompt ">"
-set --global hydro_symbol_git_dirty " x"
-
-set --global hydro_multiline true
-
-  # Aliases
+  ### ALIASES
+  # general
   alias :q "exit"
+  alias :qa "exit"
   alias cp "rsync -avSAXHP"
   alias dd "dd status=progress"
   alias eza "eza --icons --long --all"
   alias yay "yay --sudoloop"
   alias fixmypdf "ocrmypdf -l deu+eng+fra --rotate-pages --clean --clean-final --skip-text --optimize 2 --jpeg-quality 70 --png-quality 70 --jbig2-lossy"
 
-  # git Aliases
+  # git
   alias gc "git commit -m"
   alias gd "git diff"
   alias gl "git log --pretty=format:'%h - %an : %s'"
