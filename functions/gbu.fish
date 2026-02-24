27205a9b1ce3f6630b8a0c39ad725c3f17dc469d
AkytharPandaaa <akytharpandaaa@posteo.net>
2026-02-24 09:00:16 +0100

    functions/gbu.fish: fixed issue when there are multiple remotes (now chooses first)

--- functions/gbu.fish
+++ functions/gbu.fish
@@ -1,5 +1,5 @@
 function gbu
-  set remote_name $(git remote)
+  set remote_name $(git remote | head -n1)
   set branch_name $(git branch | grep -Po "(?<=\* ).+")
 
   echo "running: git branch --set-upstream-to=$remote_name/$branch_name $branch_name"
