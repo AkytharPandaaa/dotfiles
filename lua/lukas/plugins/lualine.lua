ddf96f45476147fbfc236e84576ca542b1ec801d
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-04-30 06:07:10 +0200

    lualine.lua: changed theme to rose-pine, added lsp_status

--- lua/lukas/plugins/lualine.lua
+++ lua/lukas/plugins/lualine.lua
@@ -8,17 +8,33 @@ return {
 		-- configure lualine with modified theme
 		lualine.setup({
 			options = {
-				theme = "catppuccin",
+				theme = "rose-pine",
 			},
 			sections = {
 				lualine_x = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
+						color = { fg = "#c4a7e7" },
 					},
 					{ "encoding" },
 					{ "fileformat" },
+					{
+						"lsp_status",
+						icon = "", -- f013
+						symbols = {
+							-- Standard unicode symbols to cycle through for LSP progress:
+							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+							-- Standard unicode symbol for when LSP is done:
+							done = "✓",
+							-- Delimiter inserted between LSP names:
+							separator = " ",
+						},
+						-- List of LSP names to ignore (e.g., `null-ls`):
+						ignore_lsp = { "stylua", "harper_ls" },
+						-- Display the LSP name
+						show_name = true,
+					},
 					{ "filetype" },
 				},
 			},
