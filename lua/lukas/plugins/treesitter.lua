baca3d32fa9404d5f767af65a3d06ab6a3b793f3
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-05-01 07:00:03 +0200

    treesitter.lua: added folding

--- lua/lukas/plugins/treesitter.lua
+++ lua/lukas/plugins/treesitter.lua
@@ -15,8 +15,10 @@ return {
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = { "<filetype>" },
 			callback = function()
-				vim.treesitter.start()
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.wo[0][0].foldmethod = "expr"
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				vim.treesitter.start()
 			end,
 		})
 
