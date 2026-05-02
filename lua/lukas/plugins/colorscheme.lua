3c1ade19e98718ac69fb5974f9ec8347c0d03541
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-05-02 10:11:30 +0200

    colorscheme.lua: removed unused colorschemes

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -19,33 +19,6 @@ return {
 					which_key = true,
 				},
 			})
-			vim.cmd([[colorscheme catppuccin-macchiato]])
-		end,
-	},
-	-- ===========================================================
-	{ -- https://github.com/ribru17/bamboo.nvim
-		"ribru17/bamboo.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("bamboo").setup({
-				code_style = {
-					comments = { italic = false },
-					conditionals = { italic = false },
-					namespaces = { italic = false },
-					parameters = { italic = false },
-				},
-			})
-		end,
-	},
-	{ -- https://github.com/sainnhe/edge
-		"sainnhe/edge",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.edge_disable_italic_comment = true
-			vim.g.edge_style = "light"
-			vim.g.edge_better_performance = true
 		end,
 	},
 	{ -- https://github.com/sainnhe/everforest
@@ -56,60 +29,6 @@ return {
 			vim.g.everforest_background = "soft"
 			vim.g.everforest_disable_italic_comment = true
 			vim.g.everforest_better_performance = true
-		end,
-	},
-	{ -- https://github.com/projekt0n/github-nvim-theme
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		priority = 1000,
-		lazy = false,
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = false,
-					styles = {
-						comments = "NONE",
-						functions = "bold",
-						keywords = "bold",
-						variables = "NONE",
-						conditionals = "bold",
-						constants = "NONE",
-						numbers = "NONE",
-						operators = "NONE",
-						strings = "NONE",
-						types = "bold",
-					},
-				},
-			})
-		end,
-	},
-	{ -- https://github.com/Skardyy/makurai-nvim
-		"Skardyy/makurai-nvim",
-		config = function()
-			-- you don't have to call setup
-			require("makurai").setup({
-				transparent = false, -- removes the bg color
-				bordered = false, -- removes the bg color from floats/popups
-				increase_contrast = false, -- only changes the line number and active line number for now.
-			})
-		end,
-	},
-	{ -- https://github.com/shaunsingh/nord.nvim
-		"shaunsingh/nord.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.nord_italic = false
-		end,
-	},
-	{ -- https://github.com/AlexvZyl/nordic.nvim
-		"AlexvZyl/nordic.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("nordic").setup({
-				italic_comments = false,
-			})
 		end,
 	},
 	{ -- https://github.com/pauchiner/pastelnight.nvim
@@ -139,16 +58,7 @@ return {
 					transparent = true,
 				},
 			})
-		end,
-	},
-	{ -- https://github.com/sainnhe/sonokai
-		"sainnhe/sonokai",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.sonokai_style = "maia"
-			vim.g.sonokai_disable_italic_comment = true
-			vim.g.sonokai_better_performance = true
+			vim.cmd([[colorscheme rose-pine-moon]])
 		end,
 	},
 	{ -- https://github.com/folke/tokyonight.nvim
@@ -164,7 +74,4 @@ return {
 			})
 		end,
 	},
-	{ "mhartington/oceanic-next", priority = 1000 }, -- https://github.com/mhartington/oceanic-next
-	{ "Mofiqul/dracula.nvim" }, -- https://github.com/Mofiqul/dracula.nvim
-	{ "perpetuatheme/nvim", name = "perpetua" }, -- https://github.com/perpetuatheme/nvim
 }
