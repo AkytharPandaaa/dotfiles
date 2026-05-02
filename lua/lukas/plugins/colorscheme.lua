cb98b817b0875f2e6c8ac7b2a2ff42f9e97b48b0
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-05-02 21:03:26 +0200

    colorscheme: rose-pine: changed italic to false, added other defaults from gh

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -52,10 +52,49 @@ return {
 		priority = 1000,
 		config = function()
 			require("rose-pine").setup({
+				variant = "auto", -- auto, main, moon or dawn
+				dark_variant = "moon", -- main, moon or dawn
+
+				enable = {
+					terminal = true,
+					migrations = true, -- handle deprecated options automatically
+				},
+
 				styles = {
 					bold = true,
-					italic = true,
-					transparent = false,
+					italic = false,
+					transparent = false, -- not working in moon on linux with kitty
+				},
+
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
+
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
+
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
 				},
 			})
 			vim.cmd([[colorscheme rose-pine-moon]])
