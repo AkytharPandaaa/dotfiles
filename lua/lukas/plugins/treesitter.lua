5a654a2254ca754975ef7cdddf01a7afb4b0d950
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-05-02 13:14:12 +0200

    treesitter: found working version, removed unneeded parsers

--- lua/lukas/plugins/treesitter.lua
+++ lua/lukas/plugins/treesitter.lua
@@ -11,58 +11,64 @@ return {
 		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter")
 
-		-- highlighting
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "<filetype>" },
-			callback = function()
-				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-				vim.wo[0][0].foldmethod = "expr"
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				vim.treesitter.start()
-			end,
-		})
-
-		-- pre-installed language parsers
-		treesitter.install({
+		local parsers = {
 			"angular",
 			"arduino",
 			"bash",
 			"bibtex",
 			"c",
 			"c_sharp",
-			"cpp",
+			"comment",
 			"css",
+			"csv",
+			"desktop",
 			"devicetree",
 			"dockerfile",
-			"gdscript",
+			"fish",
 			"gitignore",
 			"gpg",
-			"graphql",
-			"fish",
 			"html",
-			"javascript",
+			"hyprlang",
 			"json",
-			"kotlin",
 			"latex",
 			"lua",
 			"markdown",
-			"markdown_inline",
 			"passwd",
-			"php",
 			"powershell",
-			"prisma",
 			"python",
-			"query",
 			"regex",
+			"requirements",
 			"rust",
 			"sql",
 			"ssh_config",
-			"svelte",
-			"tsx",
-			"typescript",
+			"sway",
+			"tmux",
+			"udev",
+			"unison",
 			"vim",
 			"vimdoc",
+			"vue",
 			"yaml",
+			"zsh",
+		}
+
+		-- highlighting
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = parsers,
+			callback = function()
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.wo[0][0].foldmethod = "expr"
+
+				-- Optional: configure fold behavior - Initial fold level (0 = all closed, 99 = all open)
+				vim.wo.foldlevel = 99 -- Start with all folds open
+				-- vim.wo.foldlevelstart = 99 -- Crashes the plugin *shrug*
+
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				vim.treesitter.start()
+			end,
 		})
+
+		-- pre-installed language parsers
+		treesitter.install(parsers)
 	end,
 }
