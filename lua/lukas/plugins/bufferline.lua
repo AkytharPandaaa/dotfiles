95ab51d5e1b7e0f42f87ee08efa4718eedbbd392
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-05-02 10:05:11 +0200

    bufferline.lua: cleanup

--- lua/lukas/plugins/bufferline.lua
+++ lua/lukas/plugins/bufferline.lua
@@ -6,22 +6,6 @@ return {
 		options = {
 			mode = "tabs", -- or buffers
 			separator_style = "slant",
-			-- 			custom_areas = {
-			-- 				right = function()
-			-- 					local result = {}
-			-- 					local seve = vim.diagnostic.severity
-			-- 					local info = #vim.diagnostic.get(0, { severity = seve.INFO })
-			-- 					local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
-			--
-			-- 					if error ~= 0 then
-			-- 						table.insert(result, { text = "ERROR" .. error, link = "DiagnosticError" })
-			-- 					end
-			--
-			-- 					if info ~= 0 then
-			-- 						table.insert(result, { text = "INFO" .. info, link = "DiagnosticInfo" })
-			-- 					end
-			-- 				end,
-			-- 			},
 		},
 	},
 }
