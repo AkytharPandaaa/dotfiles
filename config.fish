909097ad1cf4c49a7a4f750e43296f89550c01ce
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-05-27 09:44:50 +0200

    config.fish: restructured, added plugin settings for magic-enter, added automatic starship theme setup, added ls alias for eza

--- config.fish
+++ config.fish
@@ -1,45 +1,60 @@
 if status is-interactive
-    # theme
+
+    # ===============================
+    # THEME
+    # ===============================
     fish_config theme choose --color-theme=dark "Rosé Pine Moon Auto"
+
+    # starship theme
+    if command -q starship && test -e ~/.config/fish/plugins/starship-rose-pine/rose-pine-moon.toml && ! test -e ~/.config/starship.toml
+        ln -s ~/.config/fish/plugins/starship-rose-pine/rose-pine-moon.toml ~/.config/starship.toml
+    end
+
+    # ===============================
+    # PLUGINS
+    # ===============================
 
     # install PandaFishFun (custom functions)
     if test -e "./scripts/PandaFishFun/functions/_panfishfun_universal_os_update.fish"
         fisher install ./scripts/PandaFishFun/
     end
 
-    # theme setup: ln -s ~/.config/fish/plugins/starship-rose-pine/rose-pine-moon.toml ~/.config/starship.toml
-
-    #set --global hydro_color_pwd "#$(echo $fish_color_gold | grep -Po [0-9af]{6})"
-    # Rosé Pine colors
-    # fish_color_subtle #908caa
-    # fish_color_text #e0def4
-    # fish_color_love #eb6f92
-    # fish_color_gold #f6c177
-    # fish_color_rose #ea9a97
-    # fish_color_pine #3e8fb0
-    # fish_color_foam #9ccfd8
-    # fish_color_iris #c4a7e7
+    # magic-enter
+    function magic-enter-cmd --description "Print the command to run when no command was given"
+        set -l cmd ls
+        if command git rev-parse --is-inside-work-tree &>/dev/null
+            set cmd "git pull; git status -sb; echo ''; ls"
+        end
+        echo $cmd
+    end
 
     # sponge
     set sponge_successful_exit_codes 0 127 255
     set sponge_purge_only_on_exit true
 
-    ### ALIASES
+    # ===============================
+    # ALIASES
+    # ===============================
+
     # general
     alias :qa exit
     alias dd "dd status=progress"
-    alias dskspeed "sudo echo \"running write test:\" && sudo dd bs=100M count=50 oflag=dsync if=/dev/zero"
-    alias eza "eza --icons"
+    alias dskspeed "sudo echo 'running write test:' && sudo dd bs=100M count=50 oflag=dsync if=/dev/zero"
+    alias eza "eza --icons --oneline"
     alias fixmypdf "ocrmypdf -l deu+eng+fra --rotate-pages --clean --clean-final --skip-text --optimize 2 --jpeg-quality 70 --png-quality 70 --jbig2-lossy"
+    alias ls "ls -1"
     alias rm "rm -v"
     alias rsync "rsync -avSAXHP"
     alias yay "yay --sudoloop"
+
+    if command -q eza
+        alias ls eza
+    end
 
     # git
     alias gc "git commit -m"
     alias gd "git diff"
     alias gl "git log --pretty=format:'%h - %an : %s'"
-    alias gll "git log --pretty=format:'%h' | head -n1"
     alias gp "git pull && git push"
     alias gs "git status -sb"
     alias gsubinit "git submodule update --init --recursive"
