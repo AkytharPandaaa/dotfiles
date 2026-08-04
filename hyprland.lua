9d584572bbc6a3ae15999c9862d2c5d5db272d48
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-08-04 06:29:39 +0200

    structure: initial commit

---
+++ hyprland.lua
@@ -0,0 +1,17 @@
+-- hyprland configuration by AkytharPandaaa
+require("lua.aky.programs")
+
+require("lua.aky.monitors")
+require("lua.aky.workspaces")
+
+require("lua.aky.variables")
+
+require("lua.aky.input")
+require("lua.aky.keybinds")
+
+require("lua.aky.design")
+
+require("lua.aky.autostart")
+require("lua.aky.tricks")
+
+require("lua.aky.windowrules")
