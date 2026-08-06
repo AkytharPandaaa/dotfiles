31599c08d1d1fd599f733b4654dd8db9252696fd
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-08-06 09:46:26 +0200

    monitors: adds fancy section title

--- lua/aky/monitors.lua
+++ lua/aky/monitors.lua
@@ -1,4 +1,8 @@
--- --- separate config for the wide screen ---
+-- ------------------------------------------------------------
+-- MONITORS
+-- ------------------------------------------------------------
+
+-- separate config for the wide screen (ultra wide/wide)
 -- hl.monitor({ output = "DP-2", mode = "3440x1440@59.97", position = "1080x0", scale = 1 })
 hl.monitor({ output = "DP-2", mode = "2560x1440@59.97", position = "1080x0", scale = 1 })
 hl.monitor({ output = "DP-3", mode = "1920x1080@60", position = "0x0", scale = 1, transform = 1 })
