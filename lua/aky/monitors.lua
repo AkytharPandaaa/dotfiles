2c67b43ef9ef6e58013d49ac2ba00721ede44934
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-08-06 09:40:37 +0200

    monitors: cleanup

--- lua/aky/monitors.lua
+++ lua/aky/monitors.lua
@@ -1,7 +1,6 @@
 -- --- separate config for the wide screen ---
 -- hl.monitor({ output = "DP-2", mode = "3440x1440@59.97", position = "1080x0", scale = 1 })
 hl.monitor({ output = "DP-2", mode = "2560x1440@59.97", position = "1080x0", scale = 1 })
-
 hl.monitor({ output = "DP-3", mode = "1920x1080@60", position = "0x0", scale = 1, transform = 1 })
 
 -- default config
