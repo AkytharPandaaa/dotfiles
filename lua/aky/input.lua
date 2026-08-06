319f4d6c5569ef06c0d9476c6ad99b6cd0bbf931
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-08-06 08:36:21 +0200

    input: adds initial config

---
+++ lua/aky/input.lua
@@ -0,0 +1,31 @@
+hl.config({
+	input = {
+		kb_layout = "de",
+
+		numlock_by_default = true,
+
+		repeat_rate = 25,
+		repeat_delay = 600,
+
+		sensitivity = 0.0,
+		accel_profile = "flat",
+		natural_scroll = false,
+
+		follow_mouse = 1,
+
+		focus_on_close = 1,
+
+		touchpad = {
+			disable_while_typing = true,
+
+			natural_scroll = true,
+			tap_to_click = true,
+		},
+
+		tablet = { -- device: wacom intuos s pen
+			-- TODO: move into devices file
+			transform = 0,
+			output = "DP-2",
+		},
+	},
+})
