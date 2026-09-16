4ab94035dd1691afdb2e715953632e641704e51c
Lukas|@AkytharPandaaa <akytharpandaaa@posteo.net>
2026-09-16 09:30:52 +0200

    monitors: changes to HD (720p) resolution

--- lua/aky/monitors.lua
+++ lua/aky/monitors.lua
@@ -3,7 +3,7 @@
 -- ------------------------------------------------------------
 
 -- separate config for the res on the main screen
-hl.monitor({ output = "eDP-1", mode = "1920x1200@60", position = "0x0", scale = 1.25 })
+--hl.monitor({ output = "eDP-1", mode = "1920x1200@60", position = "0x0", scale = 1.25 })
 hl.monitor({ output = "eDP-1", mode = "1280x720@60", position = "0x0", scale = 1 })
 
 -- default config
